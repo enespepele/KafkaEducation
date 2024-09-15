e474b3022a81574f56ed1e1217d665dd8dee32d9
enes pepele <enesppl2@gmail.com>
2024-09-15 14:17:54 +0300

    For Send is work

--- src/main/java/com/enesppl/kafka/KafkaBaseProducer.java
+++ src/main/java/com/enesppl/kafka/KafkaBaseProducer.java
@@ -4,13 +4,16 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class KafkaBaseProducer {
-
+    private static final Logger logger = LoggerFactory.getLogger(KafkaBaseProducer.class);
 
     public static void main(String[] args) {
+
 
         Properties config = new Properties();
 
@@ -23,6 +26,17 @@ public class KafkaBaseProducer {
         ProducerRecord<String,String> kafkaRecord = new ProducerRecord<String,String>("transaction_eft","Hello From Java ");
         kafkaProducer.send(kafkaRecord);
         kafkaProducer.flush();
+
+        for (int i=0 ; i<120 ; i++){
+
+            ProducerRecord<String,String> kafkaRecord2 = new ProducerRecord<String,String>("transaction_eft","Hello From Java "+i);
+            kafkaProducer.send(kafkaRecord2);
+            logger.info("Hello From Java "+i+" Send to Kafka");
+            kafkaProducer.flush();
+
+        }
+
+
 
        // config.put("bootstrap.server","localhost:9092"); This is the another method for bootstrap server
 
